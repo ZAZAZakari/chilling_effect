8ed22b17e4523a16d06f6e46936d98168eda4ae3
ZAZAZAkari <tvbatvtv2@hotmail.com>
2017-03-19 22:50:26 +0800

    [Feature] Add static file for images

--- packages/custom/chilling_effect/app.js
+++ packages/custom/chilling_effect/app.js
@@ -6,7 +6,7 @@
 var Module = require('meanio').Module
 var path = require('path')
 var MeanStarter = new Module('meanStarter')
-
+var express = require('express');
 /*
  * All MEAN packages require registration
  * Dependency injection is used to define required modules
@@ -14,7 +14,7 @@ var MeanStarter = new Module('meanStarter')
 MeanStarter.register(function (app, users, system) {
   // Set views path, template engine and default layout
   app.set('views', path.join(__dirname, '/server/views'))
-
+  app.use(express.static(path.join(__dirname, '/public/assets/static')));
   MeanStarter.angularDependencies(['mean.system', 'mean.users'])
 
   return MeanStarter

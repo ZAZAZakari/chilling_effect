2d81eb5dfc9cdbf891675ae3ef98ec28c914cc08
ZAZAZAkari <tvbatvtv2@hotmail.com>
2017-03-26 19:53:57 +0800

    [Styling] Check in all stylesheets to show all HTML elements properly

--- packages/custom/chilling_effect/public/index.js
+++ packages/custom/chilling_effect/public/index.js
@@ -1,3 +1,8 @@
 'use strict';
 
 import './assets/css/common.css';
+import './assets/css/header.css';
+import './assets/css/feature.css';
+import './assets/css/header.css';
+import './assets/css/product.css';
+import './assets/css/targetAudience.css';
